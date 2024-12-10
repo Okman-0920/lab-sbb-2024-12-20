ce3d0cd5b6bb2624fdcfb3f58e02cc195ade0274
Okman-0920 <nohoksun94@gmail.com>
2024-12-10 10:46:36 +0900

    no action method name is must be readable

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -10,11 +10,17 @@ public class MainController {
     @GetMapping("/")
     public void home () {
         System.out.println("home");
+        this.contact();
     }
 
     // GTP http://lovalhost:8080/about
     @GetMapping("/about")
     public void about () {
         System.out.println("about");
+        this.contact();
+    }
+
+    public void contact() {
+        System.out.println("contact");
     }
 }
